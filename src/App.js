f464e8f58dc834c7d32b8745666702684459ac88
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2019-11-13 16:22:37 -0500

    portfolio scaffold

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+//components
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Projects />
+      <Experience />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Projects.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Projects.css";
+
+const Projects = () => {
+  return (
+    <div>
+      <div className="projectBody">
+        <div className="project">
+          <h3>Project Name:</h3>
+          {/* <img>Preview IMG</img> */}
+          <p>Brief Description</p>
+          <button>Link To Project</button>
+        </div>
+        <div className="project">
+          <h3>Project Name:</h3>
+          {/* <img>Preview IMG</img> */}
+          <p>Brief Description</p>
+          <button>Link To Project</button>
+        </div>
+        <div className="project">
+          <h3>Project Name:</h3>
+          {/* <img>Preview IMG</img> */}
+          <p>Brief Description</p>
+          <button>Link To Project</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
